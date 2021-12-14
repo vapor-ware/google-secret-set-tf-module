89e5977cc401a24a19c9ea0394be5c170442faf4
Sam Garrett <sam@vapor.io>
2021-12-14 11:50:42 -0800

    fix function case

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "svc_acct_email" {
-  value = (var.existing_svc_acct_email != "") ? var.existing_svc_acct_email : toString(try(google_service_account.secret_accessor[0].email, null))
+  value = (var.existing_svc_acct_email != "") ? var.existing_svc_acct_email : tostring(try(google_service_account.secret_accessor[0].email, null))
 }
